bb1c0eac19cd0571df75cdcfd502bf83e6af31ba
poojamalagi <poojamalagi00@gmail.com>
2025-09-09 22:01:15 +0530

    Add files via upload

---
+++ my 1.c
@@ -0,0 +1,14 @@
+/* Write a program to print My basic details */
+/* Program:To print my Basic details
+   Input: Name, DOB, Mobile
+   Output:pooja, 26/09/1998, 9980290117
+   Author & Data:Pooja, 30/07/2025
+   */
+   #include<stdio.h>
+   char Name[] = "Pooja";
+   char DOB[] = "26/09/1998";
+   char Mobile[] = "9980290117";
+   int main()
+   {
+       printf("Name=%s\n DOB=%s\n Mobile=%s", Name,DOB,Mobile);
+   }
