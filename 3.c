ecbe7b807f720e134e12e607ef037938a4bb7191
poojamalagi <poojamalagi00@gmail.com>
2025-09-26 02:09:14 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,9 @@
+/*WAP your full name, age and favorite color */
+#include<stdio.h>
+int main()
+{
+    printf("Name= Pooja Malagi");
+    printf("\nAge= 23");
+    printf("\nFavorite Color= Blue");
+}
+
